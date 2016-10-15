205218a1807ec7ede10260073f9ff1f9e4eff3af
AlecMaliwanag <amaliwanag@gmail.com>
2016-10-14 20:00:27 -0700

    Finish basic requirements and main functionality.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,13 +1,28 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, $location, Auth, Links) {
   // Your code here
-  $scope.data = {};
-  $scope.data.links = [];
-  // $scope.data.links = JSON.parse(Links.getAll());
-  //have a for loop that json parses each object in the array.
-  Links.getAll().then(function(data) {
-    $scope.data.links = data;
-  });
-  console.log($scope.data.links);
+  if (Auth.isAuth()) {
+    $scope.data = {};
+    $scope.data.links = [];
+    // $scope.data.links = JSON.parse(Links.getAll());
+    //have a for loop that json parses each object in the array.
+
+    // Links.getAll().then(function(data) {
+    //   $scope.data.links = data.sort(function(a, b) {
+    //     return b.visits - a.visits;
+    //   });
+    // });
+
+    Links.getAll().then(function(data) {
+      $scope.data.links = data;
+    });
+
+  } else {
+    $location.path('/signup');
+  }
+
+  $scope.signOut = function() {
+    Auth.signout();
+  };
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,9 +1,24 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  $scope.link = {};
-  // Your code here
-  $scope.addLink = function(link) {
-    Links.addOne(link);
+.controller('ShortenController', function ($scope, Auth, $location, Links) {
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+  if (Auth.isAuth()) {
+    $scope.link = {};
+    // Your code here
+    $scope.addLink = function(link) {
+      if (link.match(rValidUrl)) {
+        Links.addOne(link);
+      } else {
+        alert('not a valid url.  Make sure to use http:// OR https://');
+      }
+    };
+  } else {
+    $location.path('/signup');
+  }
+
+  $scope.signOut = function() {
+    Auth.signout();
   };
+
 });
